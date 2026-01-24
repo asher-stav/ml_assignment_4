b6a692c6f65a80060a214ff5588e0f6a0171458e
asher-stav <asherst@post.bgu.ac.il>
2026-01-24 15:36:19 +0200

    Add global config

--- global_config.py
+++ global_config.py
@@ -1,3 +1,43 @@
+"""
+A config so we could change things in one place instead of everywhere in the code
+"""
+
+import torch
+
+## ==============================================================================
+## General configs
+## ==============================================================================
+
+__builders_config = {
+    'num_of_classes': 10,
+    'device': torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    'freeze': False
+}
+
+## ==============================================================================
+## Optimizer Configs
+## ==============================================================================
+
+__adam_config = {
+    'name': 'Adam',
+    'lr': 0.001,
+}
+
+
+## ==============================================================================
+## Exported Config
+## ==============================================================================
+
 CONFIG = {
+    # determines if logs will be printed
+    'debug': False,
+
+    # i don't like the key name
+    'builders_config': __builders_config,
     
+    # Must have a 'name' key
+    'optimizer': __adam_config,
+    
+    # Criterion name
+    'criterion': 'CrossEntropyLoss'
 }
