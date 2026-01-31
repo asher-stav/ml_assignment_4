f5056d116180b76653667d9df323ff395ec0e9b3
asher-stav <asherst@post.bgu.ac.il>
2026-01-31 13:56:21 +0200

    Decrease number of epochs to 10

--- global_config.py
+++ global_config.py
@@ -12,7 +12,7 @@ __builders_config = {
     'num_of_classes': 102,
     'device': torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     'freeze': False,
-    'epochs': 16,
+    'epochs': 10,
     'batch_size': 32
 }
 
